26149a6c9ebbd919e738fdcab4024f1345b5f01f
Dominik Stańczak <stanczakdominik@gmail.com>
2019-04-12 10:50:01 +0200

    Slow but parallel version

--- DLA.py
+++ DLA.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.spatial import cKDTree
 from tqdm import tqdm as tqdm
 import random
+from multiprocessing import Pool
 
 np.random.seed(0)
 DIM = 2
@@ -23,100 +24,105 @@ def displacement(loc = 3, scale=1/10):
     else:
         raise ValueError(f"Invalid dimension {DIM}!")
 
-def iterate_particle(tree,
-                     NT = 100000,
-                     R = 0.3,
-                     Rmin = 1.2,
-                     Rmax = 5
-                     ):
-    while True:
-        particle = displacement(loc=np.linalg.norm(tree.data, axis=1).max()*1.05, scale=1/2)
-        if not tree.query_ball_point(particle, Rmin):
-            # make sure we're spawning far away from existing particles
-            break
+class DLA:
+    def __init__(self, DIM):
+        self.particles = [np.zeros(DIM)]
+        self.tree = cKDTree(self.particles)
+        self.connections = []
+        self.DIM = DIM
 
-    for i in range(NT):
-        displaced_particle = particle + displacement()
-        distance, neighbor_index = tree.query(displaced_particle, 1)
+    def generate_particle(self):
+        while True:
+            appended_particle = self.iterate_particle()
+            if appended_particle is not None:
+                return appended_particle
 
-        if distance < R:  # we have found a neighbour!
-            return displaced_particle, neighbor_index
-        elif distance > Rmax:  # Particle going too far!
-            return None
-        else:  # run next iteration
-            particle = displaced_particle
-    else:
-        # Could not find neighbor in allotted time.
-        return None
+    def iterate_particle(self,
+                         NT = 100000,
+                         R = 0.3,
+                         Rmin = 1.2,
+                         Rmax = 5
+                         ):
+        while True:
+            particle = displacement(loc=np.linalg.norm(self.particles, axis=1).max()*1.05, scale=1/2)
+            if not self.tree.query_ball_point(particle, Rmin):
+                # make sure we're spawning far away from existing particles
+                break
 
-def generate_particle_sequential(tree):
-    while True:
-        appended_particle = iterate_particle(tree)
-        if appended_particle is not None:
-            return appended_particle
+        for i in range(NT):
+            displaced_particle = particle + displacement()
+            distance, neighbor_index = self.tree.query(displaced_particle, 1)
 
-def generate_particle_parallel(tree, pool):
-    # To koniecznie trzeba zrównoleglić...
-    # można puścić 4 cząstki naraz i jeśli któraś nie jest None, wybrać losową z nie-None
-    # parallel_iterate = lambda *args, **kwargs: iterate_particle(tree)
-    # trial_particles = p.map(parallel_iterate, range(4))
-    # if len(trial_partic
-    func = lambda *args, **kwargs: iterate_particle(tree)
-    while True:
-        particles = list(filter(None, pool.map(func, range(pool._processes))))
-        if len(particles):
-            return random.choice(particles)
-
-def make_fractal(N_particles = N_particles, N_procs=None):
-    particles = [np.zeros(DIM)]
-    tree = cKDTree(particles)
-    connections = []
-    if N_procs is not None:
-        p = Pool(N_procs)
-
-    for N in tqdm(range(N_particles)):
-        if N_procs is not None:
-            particle, neighbor_index = generate_particle_parallel(tree, p)
+            if distance < R:  # we have found a neighbour!
+                return displaced_particle, neighbor_index
+            elif distance > Rmax:  # Particle going too far!
+                return None
+            else:  # run next iteration
+                particle = displaced_particle
         else:
-            particle, neighbor_index = generate_particle_sequential(tree)
-        particles.append(particle)
-        tree = cKDTree(particles)
-        connections.append((N+1, neighbor_index))
+            # Could not find neighbor in allotted time.
+            return None
 
-    if N_procs is not None:
-        p.close()
+    def make_fractal(self, N_particles = N_particles, ):
+        for N in tqdm(range(N_particles)):
+            particle, neighbor_index = self.generate_particle()
+            self.particles.append(particle)
+            self.tree = cKDTree(self.particles)
+            self.connections.append((N+1, neighbor_index))
 
-    particles = np.vstack(particles)
-    return particles, connections
-
-def plot_positions(particles, connections):
-    lines = [(particles[par1], particles[par2]) for par1, par2 in connections]
-    if DIM == 2:
+    def plot_mass_distribution(self):
         import matplotlib.pyplot as plt
-        x, y = particles.T
-        # plt.scatter(x, y)
-        for (x1, y1), (x2, y2) in lines:
-            plt.plot([x1, x2], [y1, y2]) 
-        plt.grid()
+        distances = np.linalg.norm(self.particles, axis=1)
+        Rs = np.linspace(0, distances.max(), 100)
+        Ns = [len(self.tree.query_ball_point(np.zeros(DIM), R)) for R in Rs]
+        plt.plot(Rs, Ns)
         plt.show()
-    elif DIM == 3:
-        from mayavi import mlab
-        x, y, z = particles.T
-        for (x1, y1, z1), (x2, y2, z2) in lines:
-            mlab.plot3d([x1, x2], [y1, y2], [z1, z2]) 
-        mlab.show()
 
-def plot_mass_distribution(particles):
-    import matplotlib.pyplot as plt
-    tree = cKDTree(particles)
-    distances = np.linalg.norm(particles, axis=1)
-    Rs = np.linspace(0, distances.max(), 100)
-    Ns = [len(tree.query_ball_point(np.zeros(DIM), R)) for R in Rs]
-    plt.plot(Rs, Ns)
-    plt.show()
+    def plot_positions(self):
+        lines = [(self.particles[par1], self.particles[par2]) for par1, par2 in self.connections]
+        if self.DIM == 2:
+            import matplotlib.pyplot as plt
+            x, y = np.vstack(self.particles).T
+            # plt.scatter(x, y)
+            for (x1, y1), (x2, y2) in lines:
+                plt.plot([x1, x2], [y1, y2]) 
+            plt.grid()
+            plt.show()
+        elif self.DIM == 3:
+            from mayavi import mlab
+            x, y, z = np.vstack(self.particles).T
+            for (x1, y1, z1), (x2, y2, z2) in lines:
+                mlab.plot3d([x1, x2], [y1, y2], [z1, z2]) 
+            mlab.show()
+        else:
+            raise NotImplementedError(self.DIM)
 
+class MapDLA(DLA):
+    def __init__(self, DIM, N_procs):
+        super().__init__(DIM)
+        self.N_procs = N_procs
+
+    def generate_particle(self, pool):
+        # To koniecznie trzeba zrównoleglić...
+        # można puścić 4 cząstki naraz i jeśli któraś nie jest None, wybrać losową z nie-None
+        # parallel_iterate = lambda *args, **kwargs: iterate_particle(tree)
+        # trial_particles = p.map(parallel_iterate, range(4))
+        # if len(trial_partic
+        while True:
+            particles = list(filter(None, pool.map(self.iterate_particle, range(pool._processes))))
+            if len(particles):
+                return random.choice(particles)
+
+    def make_fractal(self, N_particles = N_particles):
+        with Pool(self.N_procs) as p:
+            for N in tqdm(range(N_particles)):
+                particle, neighbor_index = self.generate_particle(p)
+                self.particles.append(particle)
+                self.tree = cKDTree(self.particles)
+                self.connections.append((N+1, neighbor_index))
 
 if __name__ == "__main__":
-    particles, connections = make_fractal(N_particles, N_procs = 4)
-    plot_positions(particles, connections)
-    # plot_mass_distribution(particles)
+    d = DLA(2)
+    # d = MapDLA(2, 4)
+    d.make_fractal()
+    d.plot_positions()
