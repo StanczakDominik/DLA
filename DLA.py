697ce78f51074d968a0593fd59de359172954659
Dominik Stańczak <stanczakdominik@gmail.com>
2019-04-15 14:30:29 +0200

    2D, 3D classes, dimension fitting

--- DLA.py
+++ DLA.py
@@ -3,33 +3,25 @@ from scipy.spatial import cKDTree
 from tqdm import tqdm as tqdm
 import random
 from multiprocessing import Pool
+import json
 
-np.random.seed(0)
-DIM = 2
 N_particles = 5000
 
-def displacement(loc = 3, scale=1/10):
-    distance = np.abs(np.random.normal(loc=loc, scale=scale))
-    theta= np.random.random()*2*np.pi
-    if DIM == 3:
-        phi = np.random.random() * np.pi    
-        x = np.cos(theta) * np.sin(phi) * distance
-        y = np.sin(theta) * np.sin(phi) * distance
-        z = np.cos(phi) * distance
-        return np.array([x, y, z])
-    elif DIM == 2:
+
+class DLA2D:
+    DIM = 2
+    @classmethod
+    def displacement(cls, loc = 3, scale=1/10):
+        distance = np.abs(np.random.normal(loc=loc, scale=scale))
+        theta= np.random.random()*2*np.pi
         x = np.cos(theta) * distance
         y = np.sin(theta) * distance
         return np.array([x, y])
-    else:
-        raise ValueError(f"Invalid dimension {DIM}!")
 
-class DLA:
-    def __init__(self, DIM):
-        self.particles = [np.zeros(DIM)]
+    def __init__(self):
+        self.particles = [np.zeros(self.DIM)]
         self.tree = cKDTree(self.particles)
         self.connections = []
-        self.DIM = DIM
 
     def generate_particle(self):
         while True:
@@ -44,13 +36,13 @@ class DLA:
                          Rmax = 5
                          ):
         while True:
-            particle = displacement(loc=np.linalg.norm(self.particles, axis=1).max()*1.05, scale=1/2)
+            particle = self.displacement(loc=np.linalg.norm(self.particles, axis=1).max()*1.05, scale=1/2)
             if not self.tree.query_ball_point(particle, Rmin):
                 # make sure we're spawning far away from existing particles
                 break
 
         for i in range(NT):
-            displaced_particle = particle + displacement()
+            displaced_particle = particle + self.displacement()
             distance, neighbor_index = self.tree.query(displaced_particle, 1)
 
             if distance < R:  # we have found a neighbour!
@@ -73,41 +65,79 @@ class DLA:
     def plot_mass_distribution(self):
         import matplotlib.pyplot as plt
         distances = np.linalg.norm(self.particles, axis=1)
-        Rs = np.linspace(0, distances.max(), 100)
-        Ns = [len(self.tree.query_ball_point(np.zeros(DIM), R)) for R in Rs]
-        plt.plot(Rs, Ns)
+        minimum = -2
+        maximum = np.log(distances.max())
+        span = maximum - minimum
+        R1 = np.exp(0.1 * span + minimum)
+        R2 = np.exp(0.9 * span + minimum)
+        Rs = np.logspace(minimum, maximum, 1000)
+        Ns = np.array([len(self.tree.query_ball_point(np.zeros(self.DIM), R)) for R in Rs])
+        indices = (R1 < Rs) & (Rs < R2)
+        plt.loglog(Rs, Ns)
+        a_fit, b_fit = np.polyfit(np.log(Rs[indices]), np.log(Ns[indices]), 1)
+        title = f"Estimated dimension: {a_fit:.3f}"
+        plt.title(title)
+        plt.loglog(Rs[indices], Ns[indices], "r", label=title)
+        plt.axvline(R1, color='r', linestyle="--")
+        plt.axvline(R2, color='r', linestyle="--")
+        plt.legend(loc='best')
+        plt.xlabel("Radius R")
+        plt.ylabel("Number of particles within R")
+
         plt.show()
 
     def plot_positions(self):
+        import matplotlib.pyplot as plt
         lines = [(self.particles[par1], self.particles[par2]) for par1, par2 in self.connections]
-        if self.DIM == 2:
-            import matplotlib.pyplot as plt
-            x, y = np.vstack(self.particles).T
-            # plt.scatter(x, y)
-            for (x1, y1), (x2, y2) in lines:
-                plt.plot([x1, x2], [y1, y2]) 
-            plt.grid()
-            plt.show()
-        elif self.DIM == 3:
-            from mayavi import mlab
-            x, y, z = np.vstack(self.particles).T
-            for (x1, y1, z1), (x2, y2, z2) in lines:
-                mlab.plot3d([x1, x2], [y1, y2], [z1, z2]) 
-            mlab.show()
-        else:
-            raise NotImplementedError(self.DIM)
+        # x, y = np.vstack(self.particles).T
+        # plt.scatter(x, y)
+        for (x1, y1), (x2, y2) in tqdm(lines):
+            plt.plot([x1, x2], [y1, y2]) 
+        plt.grid()
+        plt.show()
 
-class MapDLA(DLA):
-    def __init__(self, DIM, N_procs):
-        super().__init__(DIM)
+    def save(self, filename):
+        with open(filename, "w") as f:
+            d = dict(particles=np.vstack(self.particles).tolist(), connections=self.connections)
+            json.dump(d, f)
+
+    @classmethod
+    def load(cls, filename):
+        with open(filename, "r") as f:
+            d = json.load(f)
+        dla = cls()
+        dla.particles = np.array(d['particles'])
+        dla.connections = d['connections']
+        dla.tree = cKDTree(dla.particles)
+        return dla
+
+class DLA3D(DLA2D):
+    DIM=3
+    @classmethod
+    def displacement(cls, loc = 3, scale=1/10):
+        distance = np.abs(np.random.normal(loc=loc, scale=scale))
+        theta= np.random.random()*2*np.pi
+        phi = np.random.random() * np.pi    
+        x = np.cos(theta) * np.sin(phi) * distance
+        y = np.sin(theta) * np.sin(phi) * distance
+        z = np.cos(phi) * distance
+        return np.array([x, y, z])
+    
+    def plot_positions(self):
+        from mayavi import mlab
+        lines = [(self.particles[par1], self.particles[par2]) for par1, par2 in self.connections]
+        x, y, z = np.vstack(self.particles).T
+        for (x1, y1, z1), (x2, y2, z2) in lines:
+            mlab.plot3d([x1, x2], [y1, y2], [z1, z2]) 
+        mlab.show()
+
+
+class MapDLA(DLA2D):
+    def __init__(self, N_procs):
+        super().__init__()
         self.N_procs = N_procs
 
     def generate_particle(self, pool):
-        # To koniecznie trzeba zrównoleglić...
-        # można puścić 4 cząstki naraz i jeśli któraś nie jest None, wybrać losową z nie-None
-        # parallel_iterate = lambda *args, **kwargs: iterate_particle(tree)
-        # trial_particles = p.map(parallel_iterate, range(4))
-        # if len(trial_partic
         while True:
             particles = list(filter(None, pool.map(self.iterate_particle, range(pool._processes))))
             if len(particles):
@@ -122,7 +152,7 @@ class MapDLA(DLA):
                 self.connections.append((N+1, neighbor_index))
 
 if __name__ == "__main__":
-    d = DLA(2)
-    # d = MapDLA(2, 4)
-    d.make_fractal()
-    d.plot_positions()
+    d = DLA3D()
+    d.make_fractal(int(1e5))
+    d.save("3d.json")
+    d.plot_mass_distribution()
